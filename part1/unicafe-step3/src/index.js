bee4550138e71ee28a8f6d64bf08fafdac009db1
ozakman <ozgen.akman@iki.fi>
2020-06-29 17:42:44 +0300

    Refactoring the application, statistics are in the own Statistics component

---
+++ part1/unicafe-step3/src/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) =>{
+  return(
+    <div>
+      <h2>{props.headline}</h2>
+      <h2>{props.secondHeadline}</h2>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>good {props.good} </p>
+      <p>neutral {props.neutral} </p>
+      <p>bad {props.bad} </p>
+      <p>all {props.good + props.neutral + props.bad} </p>
+      <p>average {(props.good * 1 + props.neutral * 0 + props.bad * -1) / (props.good + props.neutral + props.bad)} </p>
+      <p>positive {props.good / (props.good + props.neutral + props.bad) * 100} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const headline = 'give feedback'
+  const secondHeadline = 'statistics'
+
+  return (
+    <div>
+      <Header headline={headline} />
+      <button onClick={() => setGood(good + 1)} > good </button>
+      <button onClick={() => setNeutral(neutral + 1)}> neutral </button>
+      <button onClick={() => setBad(bad + 1)}> bad </button>
+      <Header secondHeadline={secondHeadline} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
