4b85966ff18c3f903d87032fa13395b1c8b19ed9
ozakman <ozgen.akman@iki.fi>
2020-08-10 18:16:31 +0300

    Deleting a single phonebook entry by HTTP DELETE request to the unique URL, tested with VS Code REST client plugin

---
+++ part3/PhonebookBackend-step4/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+
+
+let = persons = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  { 
+    "name": "Zapata Mapata", 
+    "number": "30-23-6423122",
+    "id": 5
+  }
+]
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+
+  app.get('/api/persons', (request, response) => {
+    console.log(persons)
+    return response.json(persons)
+  })
+
+  app.get('/:info', (request, response) => {
+    const numberOfElements = persons.length
+    console.log('numberOfElements: ', numberOfElements)
+
+    const dateNow = new Date()
+
+    if(numberOfElements === 0){
+      /* Since no data is attached to the response, we use the status 
+         method for setting the status, and the end method for responding 
+         to the request without sending any data. */
+      return response.status(404).end()
+    }else{
+      return response.end(`Phonebook has info for ${numberOfElements} people \r\n\r\n${dateNow}`)
+    }
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const person = persons.find(person => person.id === id)
+    /*const person = persons.find(person => {
+      console.log(person.id, typeof person.id, id, typeof id, person.id === id)
+      return person.id === id
+    })*/
+    console.log(person)
+
+    if(person){
+      return response.json(person)
+    }else{
+      return response.status(404).end()
+    }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    console.log(persons)
+    if(persons){
+      response.json(persons)
+    }else{
+      response.status(204).end()
+    }
+  })
+
+  const PORT = 3001
+  app.listen(PORT)
+  console.log(`Server running on port ${PORT}`)
