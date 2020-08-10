7d9ebd3be0ee94b2e9ad0331449c89f5335ecf17
ozakman <ozgen.akman@iki.fi>
2020-08-10 18:28:53 +0300

    Deleting a single phonebook entry by HTTP DELETE request to the unique URL, tested with VS Code REST client plugin

--- part3/PhonebookBackend-step4/index.js
+++ part3/PhonebookBackend-step4/index.js
@@ -22,11 +22,6 @@ let = persons = [
     "name": "Mary Poppendieck", 
     "number": "39-23-6423122",
     "id": 4
-  },
-  { 
-    "name": "Zapata Mapata", 
-    "number": "30-23-6423122",
-    "id": 5
   }
 ]
 
