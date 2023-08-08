2e91c20ee64b40a07e231af27db2ef5a37ff5ffe
ozakman <ozgen.akman@iki.fi>
2023-08-08 19:45:55 +0300

    Change the backend so that new numbers are saved to the database

---
+++ part3/PhonebookDatabase-step2/build/precache-manifest.92d4279da9f0efafcd21d07a4d007fed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17564964d37f0fa1e0ddd0a3c847b282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "352d4bef432c7f2217ce",
+    "url": "/static/css/main.8a721c9f.chunk.css"
+  },
+  {
+    "revision": "fe3a5631823be00f07da",
+    "url": "/static/js/2.db6bcc61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db6bcc61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352d4bef432c7f2217ce",
+    "url": "/static/js/main.4640e6a8.chunk.js"
+  },
+  {
+    "revision": "dbe912a687a7d64b45e6",
+    "url": "/static/js/runtime-main.908793da.js"
+  }
+]);
