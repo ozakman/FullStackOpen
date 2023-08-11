7e8c567b6c886aa169f616d9614a5d3736a1ba77
ozakman <ozgen.akman@iki.fi>
2023-08-11 22:30:03 +0300

    Delete part4 directory

--- part4/b3-BlogListTests-steps1-5/tests/palindrome.test.js
+++
@@ -1,7 +0,0 @@
-const palindrome = require('../utils/for_testing').palindrome
-
-test.skip('palindrome of saippuakauppias', () => {
-  const result = palindrome('saippuakauppias')
-
-  expect(result).toBe('saippuakauppias')
-})
