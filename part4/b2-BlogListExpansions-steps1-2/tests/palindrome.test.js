a7656c6cd8d4aeb59d1ac3760854fb8edc45fc7b
ozakman <ozgen.akman@iki.fi>
2023-08-11 14:32:05 +0300

    Write a test that verifies that the unique identifier property...

---
+++ part4/b2-BlogListExpansions-steps1-2/tests/palindrome.test.js
@@ -0,0 +1,7 @@
+const palindrome = require('../utils/for_testing').palindrome
+
+test.skip('palindrome of saippuakauppias', () => {
+  const result = palindrome('saippuakauppias')
+
+  expect(result).toBe('saippuakauppias')
+})
