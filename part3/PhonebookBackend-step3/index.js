4965c8095ebf6ca5e38cb7cc6cb065cc74a5c89a
ozakman <ozgen.akman@iki.fi>
2023-08-15 01:27:04 +0300

    Phonebook backend, step3

---
+++ part3/PhonebookBackend-step3/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+
+
+let = persons = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+
+  app.get('/api/persons', (request, response) => {
+    console.log(persons)
+    return response.json(persons)
+  })
+
+  app.get('/:info', (request, response) => {
+    const numberOfElements = persons.length
+    console.log('numberOfElements: ', numberOfElements)
+
+    const dateNow = new Date()
+
+    if(numberOfElements === 0){
+      return response.status(404).end()
+    }else{
+      return response.end(`Phonebook has info for ${numberOfElements} people \r\n\r\n${dateNow}`)
+    }
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const person = persons.find(person => person.id === id)
+    console.log(person)
+
+    if(person){
+      return response.json(person)
+    }else{
+      return response.status(404).end()
+    }
+  })
+
+  const PORT = 3001
+  app.listen(PORT)
+  console.log(`Server running on port ${PORT}`)
