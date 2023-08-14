11205f139ea249bba0a81f8b46a74e54a93e0c68
ozakman <ozgen.akman@iki.fi>
2023-08-15 01:43:39 +0300

    Phonebook database, step1

---
+++ part3/PhonebookDatabase-step1/build/precache-manifest.cd86c561965b51f8b37544283176a161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6793b64065b90e99247d113d8e1049b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c845b8dcd58048632cd",
+    "url": "/static/css/main.8a721c9f.chunk.css"
+  },
+  {
+    "revision": "fe3a5631823be00f07da",
+    "url": "/static/js/2.db6bcc61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db6bcc61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c845b8dcd58048632cd",
+    "url": "/static/js/main.16c9c673.chunk.js"
+  },
+  {
+    "revision": "dbe912a687a7d64b45e6",
+    "url": "/static/js/runtime-main.908793da.js"
+  }
+]);
---
+++ part3/PhonebookDatabase-step1/index.js
@@ -0,0 +1,155 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+const bodyParser = require('body-parser')
+const Person = require('./models/person')
+
+app.use(bodyParser.json())
+const cors = require('cors')
+app.use(cors())
+
+const morgan = require('morgan')
+app.use(morgan('tiny'))
+
+app.use(morgan(function (tokens, request, response) {
+  morgan.token('jsoncontent', function (request, response) {return JSON.stringify(request.body) })
+  return [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'), '-',
+    tokens['response-time'](request, response), 'ms',
+    tokens.jsoncontent(request, response)
+  ].join(' ')
+}))
+
+
+let = persons = []
+
+app.use(express.static('build'))
+
+  app.get('/title', (request, response) => {
+    response.send('<h1>Puhelinluettelon backend</h1>')
+  })
+
+  app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+    response.json(persons)
+    })
+  })
+
+  app.get('/api/persons/:info', (request, response) => {
+    Person.find({}).then(persons => {
+    numberOfElements = persons.length
+    console.log('Number of elements: ', numberOfElements)
+    let dateNow = new Date()
+    
+    if(numberOfElements === 0){
+      return response.status(404).end()
+    }else {
+      return response.end(`Phonebook has info for ${numberOfElements} people \r\n\r\n${dateNow}`)
+    }
+    })  
+  }) 
+
+  app.get('/api/persons/id/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+      if(person) {
+        response.json(person.toJSON())
+      }
+      else {
+        response.status(204).end()
+      }
+    })
+    .catch(error => next(error))
+  })
+
+  app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+  })
+
+  const generateRandomId = () => {
+    const max = 12765
+    const min = 101
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson.toJSON())
+      })
+      .catch(error => next(error))
+  })
+
+  app.post('/api/persons', (request, response, next) => {
+    const body = request.body
+    console.log('value of body:', body)
+
+    const checkName = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())
+
+    if (!body.name && body.number) {
+      return response.status(400).json({ 
+        error: 'Person name is missing' 
+      })
+    }else if(body.name && !body.number){
+      return response.status(400).json({ 
+        error: 'Phone number is missing' 
+      })
+    }else if(checkName){
+      return response.status(400).json({ 
+        error: 'Name is already in the phonebook, name must be unique' 
+      })
+    }
+    
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson.toJSON())
+    })
+    .catch(error => next(error))
+  })
+  
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if ((error.name === 'ValidationError') || (error.number === 'ValidationError')) {    
+      return response.status(400).json({ error: error.message })  
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
