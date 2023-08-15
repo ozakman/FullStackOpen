bbdb963bb3f4048bd1ca59278dff94ce1ffab803
ozakman <ozgen.akman@iki.fi>
2023-08-15 18:01:49 +0300

    Bloglist frontend, step5

---
+++ part5/5.5-bloglistFrontend-step5/src/components/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = (props) => {
+  const blog = props.blog
+  return ( 
+    <div>    
+      <p>{blog.title} - {blog.author} </p>
+    </div>  
+  )
+}
+
+export default Blog
