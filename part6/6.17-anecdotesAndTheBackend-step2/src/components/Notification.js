f6c26ee6b7b377d4f9c565b14e608342a3f73e4b
ozakman <ozgen.akman@iki.fi>
2023-08-16 12:55:18 +0300

    Anecdotes and the backend, step2

---
+++ part6/6.17-anecdotesAndTheBackend-step2/src/components/Notification.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import React from 'react'
+import { connect } from 'react-redux'
+import { showNotification } from '../reducers/notificationReducer'
+import { hideNotification } from '../reducers/notificationReducer'
+
+const Notification = ( props ) => {
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+
+  return (
+    <div style={style}>
+      {props.notification}
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  console.log('state is: ', state)
+  return {
+    anecdotes: state.anecdotes,
+    notification: state.notification,
+    filter: state.filter
+  }
+}
+
+const mapDispatchToProps = {
+    showNotification,
+    hideNotification
+}
+ 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Notification)
---
+++ part6/6.17-anecdotesAndTheBackend-step2/src/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
+import filterReducer from './reducers/filterReducer'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  notification: notificationReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+anecdoteService.getAll().then(anecdotes => 
+    store.dispatch(initializeAnecdotes(anecdotes))
+)
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
