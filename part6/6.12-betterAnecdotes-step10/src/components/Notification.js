7f71fd9090b687d0b4a1798b9523682ee78c1a9f
ozakman <ozgen.akman@iki.fi>
2023-08-16 12:42:03 +0300

    Better anecdotes, step10

---
+++ part6/6.12-betterAnecdotes-step10/src/components/Notification.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { showNotification } from '../reducers/notificationReducer'
+import { hideNotification } from '../reducers/notificationReducer'
+
+const Notification = ( props ) => {
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+
+  return (
+    <div style={style}>
+      {/*render here notification...*/}
+      {/*{props.store.getState().notification}*/}
+      {props.notification}
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  console.log('state is: ', state)
+  return {
+    anecdotes: state.anecdotes,
+    notification: state.notification,
+    filter: state.filter
+  }
+}
+
+const mapDispatchToProps = {
+    showNotification,
+    hideNotification
+}
+ 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Notification)
