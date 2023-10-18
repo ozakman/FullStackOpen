d49c65249a46a7ab6525d9966daac471cfe17ad0
ozakman <ozgen.akman@iki.fi>
2023-10-18 23:47:15 +0300

    7.7-countryHook

---
+++ part7/7.7-countryHook/src/components/Country.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country || country.length === 0) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  const countryObject = country[0]
+
+  return (
+    <div>
+      <h3>{countryObject.name['common']} </h3>
+      <div>capital {countryObject.capital} </div>
+      <div>population {countryObject.population}</div>
+      <img src={countryObject.flags['png']} height='100' alt={`flag of ${countryObject.name['common']}`}/>  
+    </div>
+  )
+}
+
+export default Country
